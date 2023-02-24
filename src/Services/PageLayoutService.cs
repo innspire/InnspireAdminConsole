d4e0d766ff40f1ba131ec713e2bb6d14e76933c2
Aeuke Peer Conradi <aeuke-peer@t-online.de>
2023-02-25 00:27:33 +0100

    Added basic NavBar functions

---
+++ src/Services/PageLayoutService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace InnspireAdminConsole.Services
+{
+    public class PageLayoutService
+    {
+        private readonly IJSRuntime jsRuntime;
+
+        public async Task ToggleSidebar()
+        {
+            await jsRuntime.InvokeVoidAsync("toggleSidebar");
+        }
+
+        public PageLayoutService(IJSRuntime jsRuntime)
+        {
+            this.jsRuntime = jsRuntime;
+        }
+    }
+}
---
+++ src/wwwroot/js/layout.js
@@ -0,0 +1,8 @@
+﻿function toggleSidebar() {
+    if (document.body.className.includes("sidebar-collapse")) {
+        document.body.className = document.body.className.replace("sidebar-collapse", "").trim();
+    }
+    else {
+        document.body.className = document.body.className.trim().concat(" sidebar-collapse");
+    }
+}
